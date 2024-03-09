3ce5e5752f68d66dc75d11890a18995c537195e9
Denis Kasala <deniskasala17@gmail.com>
2024-03-09 23:38:30 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace HelloWorld
+
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            Console.WriteLine(Add(35, 25));
+            Console.WriteLine(Multiply(45, 54));
+            Console.WriteLine(Substract(145, 49));
+            Console.WriteLine(Divide(49, 13));
+            Console.Read();
+        }
+
+        public static int Add(int x, int y) 
+        {
+        return x + y;
+        
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+
+        }
+
+        public static int Substract(int x, int y)
+        {
+            return x - y;
+
+        }
+
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+
+        }
+
+    }
+}
